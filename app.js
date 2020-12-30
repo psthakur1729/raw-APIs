4819d2563804579fe64e0dbe5b697f7cab576267
Prashant Singh Thakur <46480016+thakur1199@users.noreply.github.com>
2020-12-30 20:33:11 +0530

    Add files via upload
    ToDo App

--- app.js
+++ app.js
@@ -1,38 +1,29 @@
 const express = require('express');
-const https = require('https');
 const bodyParser = require('body-parser');
 const app = express();
 
+
 app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
 app.use(express.static('public'));
-app.get("/", function(req, res) {
-    res.sendFile(__dirname + "/index.html");
+
+
+var items = ["Task A", "Task B", "Task C"];
+app.get('/', function(req, res) {
+    var today = new Date();
+    var options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
+    };
+    var day = today.toLocaleDateString("en-US", options);
+    res.render("index", { weekday: day, listItem: items });
 });
 
-app.post("/", function(req, res) {
-    var city = req.body.city;
-    city = city.toUpperCase();
-    const url = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=83fc24ea8e63cdc8ac837f8047f61f72&units=metric";
-
-    https.get(url, function(APIres) {
-        // console.log(APIres.statusCode);
-        APIres.on("data", function(data) {
-            const weatherData = JSON.parse(data);
-            const temp = weatherData.main.temp;
-            const weatherDescription = weatherData.weather[0].description;
-            const icon = weatherData.weather[0].icon;
-            const iconURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
-
-            res.write("<h1>Temperature of " + city + " in degree Celcius is: " + temp + "</h1>");
-            res.write("<h1>Its " + weatherDescription + " out there</h1>");
-            res.write("<img src=" + iconURL + ">");
-            res.send();
-        });
-    });
+app.post('/', function(req, res) {
+    items.push(req.body.item);
+    res.redirect('/');
 });
-
-//REMEMBER res.send IS THE FINAL THING THAT HAPPENS IT ONLY HAPPENS ONCE but we can use res.write as a workaround to send multi responses
-
-app.listen(4040, function() {
-    console.log("Server started on port 4040...");
+app.listen(3000, function() {
+    console.log("Server running on port 3000...");
 });
